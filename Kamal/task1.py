7dd60da8f834ee8bd99cd6981fadf1623080581b
kamalakara2266 <kamalakarakml@gmail.com>
2019-10-19 07:58:18 +0530

    1st task

---
+++ Kamal/task1.py
@@ -0,0 +1,7 @@
+data = 'welcome to world of python programming :'
+print(data.count('w'))
+print(data.count('l'))
+print(data.count('x'))
+print(data.index('w'))
+print(data.count('l') , data.index('l') , data.index('l',3))
+
